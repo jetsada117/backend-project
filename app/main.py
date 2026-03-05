649c48950db07e7f7ab541c65f6f9f5d1cf9417e
jetsada117 <jornny.prombut@gmail.com>
2026-03-05 10:38:31 +0700

    delete log

--- app/main.py
+++ app/main.py
@@ -1,5 +1,3 @@
-import sys
-import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -8,26 +6,13 @@ from scalar_fastapi import get_scalar_api_reference
 from app.services.prediction_service import predictor_service
 from app.api.v1.api import api_router
 
-# --- 1. ตั้งค่า Logging บังคับให้แสดงผลออก Console (stdout) ---
-logging.basicConfig(
-    stream=sys.stdout,
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    force=True,
-)
-logger = logging.getLogger(__name__)
-# ---------------------------------------------------------
-
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    logger.info("Starting up FastAPI application...")
-
+    print("Starting up FastAPI application...")
     predictor_service.load_models()
-
     yield
-
-    logger.info("Shutting down FastAPI application...")
+    print("Shutting down FastAPI application...")
 
 
 app = FastAPI(title="fastapi", lifespan=lifespan)
@@ -37,7 +22,6 @@ app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/", response_class=PlainTextResponse)
 def index():
-    logger.info("Accessed GET / (Hello World)")
     return "Hello World!!"
 
 
